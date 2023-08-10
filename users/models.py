11560a78070b75a5bab28615d2a1e091495db262
colif74 <128209514+colif74@users.noreply.github.com>
2023-08-10 09:33:40 +0500

    Delete users directory

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class OneTimeCode(models.Model):
-    objects = None
-    code = models.CharField(max_length=10)
-    user = models.CharField(max_length=255)
